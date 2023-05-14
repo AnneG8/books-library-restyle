749ccd6cd8a461f2fee8985261b841c1989d9e5c
Anna Veselova <anna.veselova.97@mail.ru>
2023-05-14 23:04:45 +0300

    download books from tululu.org

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import requests
+from pathlib import Path
 
 
-url = "https://tululu.org/txt.php?id=32168"
+def get_url(num):
+    return f'https://tululu.org/txt.php?id={num}'
 
-response = requests.get(url)
-response.raise_for_status() 
 
-filename = 'The_Sands_of_Mars_-_Arthur_Clarke.txt'
-with open(filename, 'wb') as file:
-    file.write(response.content)
+def main():
+    books_dir = 'books'
+    Path(books_dir).mkdir(parents=True, exist_ok=True)
+
+    for num in range(1, 11):
+        response = requests.get(get_url(num))
+        response.raise_for_status() 
+        filename = f'{books_dir}\id{num}.txt'
+        with open(filename, 'wb') as file:
+            file.write(response.content)
+
+
+if __name__ == '__main__':
+    main()
