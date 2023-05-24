5352d9ac232019e73849abbe919b527bb28fc136
Anna Veselova <anna.veselova.97@mail.ru>
2023-05-25 01:17:19 +0300

    download books from all pages

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ from urllib.parse import urljoin, urlsplit, unquote
 import requests
 import os
 import argparse
+import re
 import time
+import json
 
 
-def check_positive(value):
+def check_non_negative(value):
     int_value = int(value)
-    if int_value <= 0:
+    if int_value < 0:
         raise argparse.ArgumentTypeError(
             "invalid positive int value: '%s'" % int_value
         )
@@ -19,28 +21,31 @@ def check_positive(value):
 
 def create_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--start_id', 
-                        type=check_positive,
+    parser.add_argument('-s', '--start_page', 
+                        type=check_non_negative,
                         default=1, const=1,   
                         nargs='?')
-    parser.add_argument('-e', '--end_id', 
-                        type=check_positive,
-                        default=10, const=10,   
+    parser.add_argument('-e', '--end_page', 
+                        type=check_non_negative,
+                        default=4, const=4,   
                         nargs='?')
     return parser
 
 
-def check_for_redirect(response):
-    if(response.history):
-        raise requests.HTTPError
-
-
-def get_page_response(num):
-    page_url = 'https://tululu.org/b{}/'
-    response = requests.get(page_url.format(num))
+def get_response(url):
+    response = requests.get(url)
     response.raise_for_status()
-    check_for_redirect(response)
     return response;
+
+
+def parse_genre_page(response):
+    soup = BeautifulSoup(response.text, 'lxml')
+    base_page_url = 'https://tululu.org/'
+    book_urls = [
+        urljoin(base_page_url, book.find('tr').find('a')['href'])
+        for book in soup.find('td', class_='ow_px_td').find_all('table')
+    ]
+    return book_urls
 
 
 def parse_book_page(response):
@@ -56,11 +61,16 @@ def parse_book_page(response):
     return {
        'title': header[0].strip(),
        'author': header[1].strip(),
-       'genres': genres,
-       'image': urljoin(response.url, image),
+       'img_url': urljoin(response.url, image),
        'comments': comments,
+       'genres': genres
     }
      
+
+def check_for_redirect(response):
+    if(response.history):
+        raise requests.HTTPError
+
 
 def download_txt(book_num, filename, folder='books/'):
     payload = {'id': book_num}
@@ -73,6 +83,12 @@ def download_txt(book_num, filename, folder='books/'):
     )
     with open(filepath, 'wb') as file:
         file.write(response.content)
+    return filepath
+
+
+def is_file_valid(filepath):
+    path = Path(filepath)
+    return (path.exists() and path.is_file())
 
 
 def download_image(url, folder='images/'):
@@ -83,6 +99,7 @@ def download_image(url, folder='images/'):
     image_path = os.path.join(folder, image_name)
     with open(image_path, 'wb') as file:
         file.write(response.content)
+    return image_path
 
 
 def main():
@@ -93,21 +110,31 @@ def main():
     parser = create_parser()
     args = parser.parse_args()
 
-    missing_pages = list()
-    for num in range(args.start_id, args.end_id + 1):
-        while True:
-            try:
-                book = parse_book_page(get_page_response(num))
-                download_txt(num, book['title'])
-                download_image(book['image'])    
-                break
-            except requests.HTTPError:
-                missing_pages.append(str(num))
-                break
-            except requests.ConnectionError:
-                time.sleep(5)
-    if missing_pages:
-        print('Не были скачаны книги: ', ','.join(missing_pages))
+    genre_page_url = 'https://tululu.org/l55/{}/'
+    books = list()
+    for page_num in range(args.start_page, args.end_page + 1):
+        book_urls = parse_genre_page(
+            get_response(genre_page_url.format(page_num))
+        )
+        for url in book_urls:
+            while True:
+                try:
+                    book = parse_book_page(get_response(url))
+                    url_path = urlsplit(unquote(url)).path
+                    num = re.sub('[/b]', '', url_path)
+                    book['book_path'] = download_txt(num, book['title'])
+                    if(is_file_valid(book['book_path'])):
+                        book['img_scr'] = download_image(book['img_url'])
+                        books.append(book)
+                    break
+                except requests.HTTPError:
+                    break
+                except requests.ConnectionError:
+                    time.sleep(5)
+    
+    book_list_json = json.dumps(books, ensure_ascii=False, indent=2)
+    with open("book_list.json", "w", encoding='utf8') as my_file:
+        my_file.write(book_list_json)
             
 
 if __name__ == '__main__':
