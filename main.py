8172e791ea22d366096cefd26adef2fc70151563
Anna Veselova <anna.veselova.97@mail.ru>
2023-05-27 23:09:28 +0300

    add some arguments

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
 from urllib.parse import urljoin, urlsplit, unquote
 import requests
-import os
 import argparse
 import re
 import time
@@ -29,7 +28,33 @@ def create_parser():
                         type=check_non_negative,
                         default=4, const=4,   
                         nargs='?')
+    parser.add_argument('-f', '--dest_folder', 
+                        type=str,
+                        default='', const='',   
+                        nargs='?')
+    parser.add_argument('-j', '--json_path', 
+                        type=str,
+                        default='', const='',   
+                        nargs='?')
+    parser.add_argument('-skt', '--skip_txt', 
+                        type=bool,
+                        default=False, const=False,   
+                        nargs='?')
+    parser.add_argument('-ski', '--skip_imgs', 
+                        type=bool,
+                        default=False, const=False,   
+                        nargs='?')
     return parser
+
+
+def prepare_dirs(dest_folder):
+    dirs = {
+        'books': Path(dest_folder) / 'books' / '',
+        'images': Path(dest_folder) / 'images' / ''
+    }
+    for file_dir in dirs:
+        Path(file_dir).mkdir(parents=True, exist_ok=True)
+    return dirs
 
 
 def get_response(url):
@@ -68,15 +93,15 @@ def check_for_redirect(response):
         raise requests.HTTPError
 
 
-def download_txt(book_num, filename, folder='books/'):
+def download_txt(book_num, filename, folder):
     payload = {'id': book_num}
     response = requests.get('https://tululu.org/txt.php', params=payload)
     response.raise_for_status()
     check_for_redirect(response)
-    filepath = os.path.join(
+    filepath = str(Path(
         folder, 
         f'{book_num}. {sanitize_filename(filename)}.txt'
-    )
+    ))
     with open(filepath, 'wb') as file:
         file.write(response.content)
     return filepath
@@ -87,24 +112,26 @@ def is_file_valid(filepath):
     return (path.exists() and path.is_file())
 
 
-def download_image(url, folder='images/'):
+def download_image(url, folder):
     response = requests.get(url)
     response.raise_for_status()
     check_for_redirect(response)
     image_name = urlsplit(unquote(url)).path.split("/")[-1]
-    image_path = os.path.join(folder, image_name)
+    image_path = str(Path(folder, image_name))
     with open(image_path, 'wb') as file:
         file.write(response.content)
     return image_path
 
 
 def main():
-    books_dirs = ['books', 'images']
-    for books_dir in books_dirs:
-        Path(books_dir).mkdir(parents=True, exist_ok=True)
-
     parser = create_parser()
     args = parser.parse_args()
+
+    try:
+        dirs = prepare_dirs(args.dest_folder)
+    except PermissionError:
+        print(f'Не хватает прав доступа для {args.dest_folder}')
+        return
 
     genre_page_url = 'https://tululu.org/l55/{}/'
     books = list()
@@ -118,9 +145,14 @@ def main():
                     book = parse_book_page(get_response(url))
                     url_path = urlsplit(unquote(url)).path
                     num = re.sub('[/b]', '', url_path)
-                    book['book_path'] = download_txt(num, book['title'])
-                    if(is_file_valid(book['book_path'])):
-                        book['img_scr'] = download_image(book['img_url'])
+                    if not args.skip_txt:
+                        book['book_path'] = \
+                            download_txt(num, book['title'], dirs['books'])
+                    if (is_file_valid(book['book_path']) 
+                        and not args.skip_imgs):
+                        book['img_scr'] = \
+                            download_image(book['img_url'], dirs['images'])
+                        del book['img_url']
                         books.append(book)
                     break
                 except requests.HTTPError:
@@ -129,7 +161,8 @@ def main():
                     time.sleep(5)
     
     book_list_json = json.dumps(books, ensure_ascii=False, indent=2)
-    with open("book_list.json", "w", encoding='utf8') as my_file:
+    json_name = Path(args.json_path, 'book_list.json')
+    with open(json_name, 'w', encoding='utf8') as my_file:
         my_file.write(book_list_json)
             
 
