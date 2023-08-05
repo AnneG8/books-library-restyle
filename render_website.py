121a059e04a8132607eb7a96aa0df888c64eecc1
Anna Veselova <anna.veselova.97@mail.ru>
2023-08-06 00:16:51 +0300

    Integrate livereload

--- render_website.py
+++ render_website.py
@@ -1,5 +1,6 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from pathlib import Path
+from livereload import Server, shell
 import argparse
 import json
 import re
@@ -31,6 +32,15 @@ def check_path(path):
     return
 
 
+def on_reload(books):
+    env = Environment(loader=FileSystemLoader('.'),
+                      autoescape=select_autoescape(['html', 'xml']))
+    template = env.get_template('template.html')
+    rendered_page = template.render(books=books)
+    with open('pages/index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+
 def main():
     parser = create_parser()
     args = parser.parse_args()
@@ -53,13 +63,12 @@ def main():
 
     with open(json_path, 'r', encoding='utf8') as file:
         books = json.load(file)
+    on_reload(books)
 
-    env = Environment(loader=FileSystemLoader('.'),
-                      autoescape=select_autoescape(['html', 'xml']))
-    template = env.get_template('template.html')
-    rendered_page = template.render(books=books)
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+    server = Server()
+    server.watch('template.html', on_reload)
+    server.serve(root='.')
+
 
 
 
